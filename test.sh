163e02f6d1a9d2f1bd83f0db39c04a92b2108e58
Aaron Lee Marais <aaron@the-marais.family>
2023-07-28 13:37:08 +0200

    update test file

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker container run --rm --entrypoint '' ${IMAGE_NAME:-aquelle1/nodejs-chrome} cat /etc/alpine-release
-docker container run --rm --entrypoint '' ${IMAGE_NAME:-aquelle1/nodejs-chrome} chromium-browser --version
+docker container run --rm --entrypoint '' ${IMAGE_NAME:-aquelle1/nodejs-chrome} google-chrome --version
